38c60bc04ed6dbad7ccc8dd84b55bb36c4cec267
Jaqueline Téllez <78996295+jaquenwn@users.noreply.github.com>
2025-07-07 13:51:20 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+   let pronoun = ['the', 'our', 'my', 'your'];
+   let adj = ['whimsical', 'magical', 'mesmerizing', 'lovely'];
+   let noun = ['space', 'night', 'garden', 'star'];
+  
+  for (let a = 0; a < 4 ; a++) {
+    let pro = pronoun[a]
+    for (let b = 0; b < 4 ; b++) {
+      let ad = adj[b]
+      for (let c = 0; c < 4 ; c++) {
+        let nou = noun[c]
+        let domain = pro + ad + nou + '.com'
+        console.log (domain)
+      }
+    }
+  }
+
 };
